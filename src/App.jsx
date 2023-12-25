ee6944af7c4e7cf6dcf92cb078c87be1e1acd6ab
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-12-26 03:01:47 +0600

    read data complete from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, push, onValue } from "firebase/database";
 
 function App() {
 
   const db = getDatabase();
+  let [text, setText] = useState("");
 
-
-
-  let [text, setText] = useState();
-
+  // write data on firebase
   let handleForm = (e) => {
     setText(e.target.value);
   }
   let handleClick = () => {
-    set(ref(db, "alltodo"), {
+    set(push(ref(db, "alltodo")), {
       todotext: text,
     })
+    setText("")
   }
+
+  let [todo, setTodo] = useState([]);
+  // Read data on firebase
+  const todoRef = ref(db, 'alltodo');
+  useEffect(() => {
+    onValue(todoRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item) => {
+        arr.push(item.val())
+      })
+      setTodo(arr)
+    });
+  }, [])
+
+
+
 
   return (
     <>
-      <input placeholder='Type something to add' onChange={handleForm} />
-      <button onClick={handleClick}>Add</button>
+      <div>
+        <input placeholder='Type something to add' value={text} onChange={handleForm} />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <ul>
+        {
+          todo.map((item, index) => (
+            <li key={index}>{item.todotext}</li>
+          ))
+        }
+      </ul>
     </>
   )
 }
